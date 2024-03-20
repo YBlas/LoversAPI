5fe7f5c4b90cd4658a91ffe4ced0ebdcef4782f3
YBlas <make202615@gmail.com>
2024-03-21 00:28:47 +0100

    love is in the air

---
+++ README.md
@@ -0,0 +1,71 @@
+## API Endpoints
+
+### GET `/`
+
+- Returns a list of all lovers in the database.
+
+### GET `/:name`
+
+- Returns lover matching the provided name.
+
+### POST `/`
+
+- Adds a new lover to the database.
+- Requires `name`, `password`, `age`, `sex`, `description`, `hobbies`, `photo`, and `comments` fields in the request body.
+- Supported image formats: jpg, jpeg, png.
+
+### PUT `/:name`
+
+- Updates lover information matching the provided name.
+- Requires the `name` field in the request parameters.
+- Requires updated lover details in the request body.
+
+### DELETE `/:name`
+
+- Deletes a lover from the database.
+- Requires the `name` field in the request parameters.
+- Requires the `password` field in the request body for authentication.
+
+### POST `/:name/comment`
+
+- Adds a comment to the lover's profile.
+- Requires `user`, `password`, and `message` fields in the request body. (User from which the comment is made)
+- Requires the `name` field in the request parameters. (User to comment on)
+- Requires the `password` field in the request body for authentication.
+
+### DELETE `/:name/comment`
+
+- Deletes comments made by a specific user from the lover's profile.
+- Requires `user`, `password` fields in the request body.
+- Requires the `name` field in the request parameters.
+- Requires the `password` field in the request body for authentication.
+
+## Example Usage
+
+### Fetch all lovers
+
+curl http://localhost:3000/
+
+### Fetch lover by name
+
+curl http://localhost:3000/:name
+
+### Add a new lover
+
+curl -X POST -H "Content-Type: application/json" -d '{"name":"John Doe","password":"pass123","age":25,"sex":"Male","description":"A nature lover","hobbies":["Traveler","Gardener"],"photo":"john_doe.jpg","comments":[]}' http://localhost:3000/
+
+### Update lover information
+
+curl -X PUT -H "Content-Type: application/json" -d '{"password":"newpass123","age":26}' http://localhost:3000/:name
+
+### Delete a lover
+
+curl -X DELETE -H "Content-Type: application/json" -d '{"password":"pass123"}' http://localhost:3000/:name
+
+### Add a comment
+
+curl -X POST -H "Content-Type: application/json" -d '{"user":"commenter1","password":"pass123","message":"Great person!"}' http://localhost:3000/:name/comment
+
+### Delete comments by a user
+
+curl -X DELETE -H "Content-Type: application/json" -d '{"user":"commenter1","password":"pass123"}' http://localhost:3000/:name/comment
---
+++ main.ts
@@ -0,0 +1,200 @@
+//@deno-types="npm:@types/express@4"
+
+import express, { Request, Response } from "npm:express";
+import {
+  Database,
+  MongoClient,
+} from "https://deno.land/x/mongo@v0.32.0/mod.ts";
+
+import { load } from "https://deno.land/std@0.218.0/dotenv/mod.ts";
+
+const env = await load();
+const password = env["PASS"];
+const user = env["USER"];
+
+const connectMongoDB = async (): Promise<Database> => {
+  const mongo_usr = user;
+  const mongo_pwd = password;
+  const db_name = "supermondongos";
+  const mongo_uri = "mongomake.3ta2r.mongodb.net";
+  const mongo_url = `mongodb+srv://${mongo_usr}:${mongo_pwd}@${mongo_uri}/${db_name}?authMechanism=SCRAM-SHA-1`;
+  const client = new MongoClient();
+  await client.connect(mongo_url);
+  const db = client.database(db_name);
+  return db;
+};
+
+const db = await connectMongoDB();
+const lovers = db.collection("lovers");
+
+enum Hobbies {
+  RECORD_COLLECTOR = "Record Collector",
+  FILM_PHOTOGRAPHY = "Film Photography",
+  BOOK_READER = "Book Reader",
+  GARDENER = "Gardener",
+  COOK = "Cook",
+  TRAVELER = "Traveler",
+  MUSICIAN = "Musician",
+  ARTIST = "Artist",
+  WRITER = "Writer",
+  COMIC_BOOK_COLLECTOR = "Comic Book Collector",
+  VINTAGE_FASHION = "Vintage Fashion",
+  ANIMAL_LOVER = "Animal Lover",
+  SPORTS_FAN = "Sports Fan",
+  MOVIE_BUFF = "Movie Buff",
+  FOODIE = "Foodie",
+  CRAFTSMAN = "Craftsman",
+  VIDEO_GAMES = "Video Games",
+  COLLECTOR = "Collector",
+  FITNESS = "Fitness",
+  DANCER = "Dancer",
+  HI_FI = "Hi-Fi",
+}
+
+type comment = {
+  user: string;
+  message: string;
+};
+
+type Lover = {
+  name: string;
+  password: string;
+  age: number;
+  sex: string;
+  description: string;
+  hobbies: Hobbies[];
+  photo: string;
+  comments: comment[];
+};
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response): void => {
+  lovers
+    .find({})
+    .toArray()
+    .then((e) => {
+      const filteredE = e.map((e) => {
+        delete e.password;
+        return e;
+      });
+      res.json(filteredE);
+    });
+});
+
+app.post("/", async (req: Request, res: Response) => {
+  const newLover: Lover = req.body;
+
+  if (
+    !newLover.name ||
+    !newLover.password ||
+    !newLover.age ||
+    !newLover.sex ||
+    !newLover.description ||
+    !newLover.hobbies ||
+    !newLover.photo ||
+    !newLover.comments ||
+    newLover.name === "" ||
+    newLover.password === ""
+  ) {
+    res.status(400).json({ error: "Missing fields" });
+    return;
+  }
+  const photo = newLover.photo;
+  const validPhoto = photo.match(/\.(jpeg|jpg|png)$/);
+  if (!validPhoto) {
+    res.status(400).json({ error: "Invalid photo" });
+    return;
+  }
+
+  const existingLover = await lovers.findOne({ name: newLover.name });
+  if (existingLover) {
+    res.status(400).json({ error: "Lover already exists" });
+    return;
+  } else {
+    await lovers.insertOne(newLover);
+    res.json(newLover);
+  }
+});
+
+app.get("/:name", async (req: Request, res: Response) => {
+  const name = req.params.name;
+  const lover = await lovers.findOne({ name });
+  if (lover) {
+    delete lover.password;
+    res.json(lover);
+  } else {
+    res.status(404).json({ error: "Lover not found" });
+  }
+});
+
+app.put("/:name", async (req: Request, res: Response) => {
+  const name = req.params.name;
+  const newLover: Lover = req.body;
+  const existingLover = await lovers.findOne({ name });
+  if (existingLover) {
+    await lovers.updateOne({ name }, { $set: newLover });
+    res.json(newLover);
+  } else {
+    res.status(404).json({ error: "Lover not found" });
+  }
+});
+
+app.delete("/:name", async (req: Request, res: Response) => {
+  const name = req.params.name;
+  const { password } = req.body;
+  const existingLover = await lovers.findOne({ name });
+  if (existingLover) {
+    if (existingLover.password === password) {
+      await lovers.deleteOne({ name });
+      res.json({ message: "Lover deleted" });
+    } else {
+      res.status(401).json({ error: "Unauthorized" });
+    }
+  } else {
+    res.status(404).json({ error: "Lover not found" });
+  }
+});
+
+app.post("/:name/comment", async (req: Request, res: Response) => {
+  const name = req.params.name;
+  const { user, password, message } = req.body;
+  const existingLover = await lovers.findOne({ name });
+  if (existingLover) {
+    if (existingLover.password === password) {
+      const comment = {
+        user,
+        message,
+      };
+      await lovers.updateOne({ name }, { $push: { comments: comment } });
+      res.json({ message: "Comment added" });
+    } else {
+      res.status(401).json({ error: "Unauthorized" });
+    }
+  } else {
+    res.status(404).json({ error: "Lover not found" });
+  }
+});
+
+app.delete("/:name/comment", async (req: Request, res: Response) => {
+  const name = req.params.name;
+  const { user, password } = req.body;
+  const existingLover = await lovers.findOne({ name });
+  if (existingLover) {
+    if (existingLover.password === password) {
+      await lovers.updateOne({ name }, { $pull: { comments: { user } } });
+      res.json({ message: "Comments deleted" });
+    } else {
+      res.status(401).json({ error: "Unauthorized" });
+    }
+  } else {
+    res.status(404).json({ error: "Lover not found" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
